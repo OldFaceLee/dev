6169b087cae8a15045ae9a23bd1e0af560c8bbb0
lixuejun <lixj5@asiainfo.com>
2020-05-26 18:36:42 +0800

    封装MocMVC controller

--- src/test/java/com/ai/dev/controller/AbstractBaseControllerTest.java
+++ src/test/java/com/ai/dev/controller/AbstractBaseControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -27,21 +28,34 @@ import java.util.Map;
 
 /**
  * @author: lixuejun
- * @date: Create in 2020/5/21
- * @description:
+ * @date: Create in 2020/5/26
+ * @description: MocMVC 的post get封装
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class)
 @Slf4j
 public abstract class AbstractBaseControllerTest {
 
-    protected MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @Autowired
-    protected WebApplicationContext context;
+    private WebApplicationContext context;
 
-    public JSONObject get(String uri){
-        return null;
+    /**
+     * Get请求
+     * @param uri
+     * @param header
+     * @return
+     */
+    public JSONObject get(String uri,Map<String,String> header){
+        String response = null;
+        try {
+            response = this.commonRequest("get",uri,header,null)
+                    .andReturn().getResponse().getContentAsString();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return JSON.parseObject(response);
     }
 
 
@@ -53,25 +67,49 @@ public abstract class AbstractBaseControllerTest {
      */
     public JSONObject post(String uri, Map<String,String> header, String jsonParam){
         String response = null;
-        ResultActions actions = null;
-        HttpHeaders httpHeaders = new HttpHeaders();
-        header.forEach((k,v)->{
-            httpHeaders.add(k,v);
-        });
-        log.info(String.format("传入的headers：%s",httpHeaders.toString()));
-        log.info(String.format("传入的jsonParam: %s",jsonParam));
         try {
-            actions = mockMvc.perform(MockMvcRequestBuilders.post(new URI(uri))
-                    .headers(httpHeaders)
-                    .accept(MediaType.APPLICATION_JSON)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(jsonParam));
-            response = actions.andReturn().getResponse().getContentAsString();
-        } catch (Exception e) {
+            response = this.commonRequest("post",uri,header,jsonParam)
+                    .andReturn().getResponse().getContentAsString();
+        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
         log.info("返回的结果"+response);
         return JSON.parseObject(response);
+    }
+
+
+    private ResultActions commonRequest(String getOrPost,String uri,Map<String,String>header,String jsonParam){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        ResultActions actions = null;
+        header.forEach((k,v)->{
+            httpHeaders.add(k,v);
+        });
+        log.info(String.format("传入的headers：%s",httpHeaders.toString()));
+        if(getOrPost.equalsIgnoreCase("post")){
+            try {
+                log.info("触发Post请求...");
+                log.info(String.format("传入的jsonParam: %s",jsonParam));
+                actions = mockMvc.perform(MockMvcRequestBuilders.post(new URI(uri))
+                        .headers(httpHeaders)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonParam));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }else if(getOrPost.equalsIgnoreCase("get")){
+            try {
+                log.info("触发Get请求...");
+                actions = mockMvc.perform(MockMvcRequestBuilders.get(new URI(uri))
+                        .headers(httpHeaders)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return actions;
+
     }
 
     @Before
